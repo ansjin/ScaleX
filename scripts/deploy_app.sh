802daddac40b3350577e867467c344a56f26b54f
Walentin L <walentinlamonos@gmail.com>
2018-05-08 15:58:18 +0200

    Removed sudos because of risk of timeouts on slow machines & connections

--- scripts/deploy_app.sh
+++ scripts/deploy_app.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
-sudo apt-get update
-sudo iptables -I INPUT -j ACCEPT
-sudo apt-get install -y docker.io
-sudo apt-get install -y curl
+# Make sure script is executed as root
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run using root (sudo)!"
+  exit
+fi
+
+apt-get update
+iptables -I INPUT -j ACCEPT
+apt-get install -y docker.io curl
 curl -L https://github.com/docker/compose/releases/download/1.13.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 cd ..
-sudo docker-compose up --build &
+docker-compose up --build &
 read -p "Press enter to Exit"
